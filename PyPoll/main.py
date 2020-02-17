93b0586acc1d1b9975b8935de9f7b6a0c9890dda
Karl Ramsay <nytechtiger@gmail.com>
2020-02-17 06:41:31 -0500

    update code to write formatted results to text file.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,6 +14,7 @@ li_percentage = 0.000
 otooley_total = 0
 otooley_percentage = 0.000
 total_votes = 0
+winner = "Not Declared"
 
 # define array structures needed
 voting = []
@@ -61,26 +62,58 @@ otooley_percentage = format(((otooley_total/total_votes) * 100), '.3f')
 li_percentage = format(((li_total/total_votes) * 100.000), '.3f')
 
 
-print(sorted_list)
+# open a new text file with "write" mode to write Election Results.
+file = open("results\\election_results.txt", "w")
 
+print()
 print("Election Results")
-print("--" * 20)
+file.write(f"Election Results\n")
+print("--" * 30)
+file.write("-" * 30)
 print(f"Total Votes: {total_votes}")
-print("--" * 20)
+file.write(f"\nTotal Votes: {total_votes}\n")
+print("--" * 30)
+print()
+file.write("-" * 30 )
 
 for s in sorted_list:
   if s == correy_total:
     print(f"Correy:  {correy_percentage}% ({correy_total})")
-  
+    print()
+    file.write(f"\nCorrey:  {correy_percentage}% ({correy_total})\n")
+
   elif s == otooley_total:
     print(f"O'Tooley: {otooley_percentage}% ({otooley_total})")
+    print()
+    file.write(f"\nO'Tooley: {otooley_percentage}% ({otooley_total})\n")
 
   elif s == khan_total:
     print(f"Khan: {khan_percentage}% ({khan_total})")
+    print()
+    file.write(f"\nKhan: {khan_percentage}% ({khan_total})\n")
 
   elif s == li_total:
     print(f"Li: {li_percentage}% ({li_total})")
+    print()
+    file.write(f"\nLi: {li_percentage}% ({li_total})\n")
 
+file.write("-" * 30 )
 
+# get the value for the winner identified!
+if correy_total == sorted_list[0]:
+  winner = "Correy"
+elif otooley_total == sorted_list[0]:
+  winner = "O'Tooley"
+elif khan_total == sorted_list[0]:
+  winner = "Khan"
+elif li_total == sorted_list[0]:
+  winner = "Li"
 
+print("-" * 30 )
+print(f"Winner: {winner}\n")
+file.write(f"\nWinner: {winner}\n")
+print("-" * 30 )
+file.write("-" * 30 )
 
+# make sure to close the file we are writing the Election Results to
+file.close()
