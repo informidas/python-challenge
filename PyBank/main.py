131f3078b14908084a2886dafb491808d640af59
Karl Ramsay <nytechtiger@gmail.com>
2020-02-17 02:23:05 -0500

    completed code to write Financial Results to a text file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -31,12 +31,38 @@ for i in range(len(pnl)):
     avg_change.append((pnl[i+1] - pnl[i]))
     delta += (pnl[i+1] - pnl[i])
 
+
 # calculate totals and averages for Financial Analysis
 total_months = len(periods)
 average_change = round((delta / (len(pnl)-1)),2)
 greatest_increase = max(avg_change)
 greatest_decrease = min(avg_change)
 
+# find the month of greatest increase
+idx = 1
+found_index = 0
+for gip in avg_change:
+    if gip == greatest_increase:
+        found_index = idx
+        #print(f"The index is: {found_index}")
+    else:
+        idx += 1
+
+# set the period associated with month of greatest increase
+greatest_increase_month = periods[found_index]
+
+# find the month of greatest decrease
+idx = 1
+found_index = 0
+for gip in avg_change:
+    if gip == greatest_decrease:
+        found_index = idx
+        #print(f"The index is: {found_index}")
+    else:
+        idx += 1
+
+# set the period associated with month of greatest decrease
+greatest_decrease_month = periods[found_index]
 
 
 # print summary with the results
@@ -45,6 +71,20 @@ print("-" * 30)
 print(f"Total Months: {total_months}")
 print(f"Total: ${profit_loss}")
 print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: (${greatest_increase})")
-print(f"Greatest Decrease in Profits: (${greatest_decrease})")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
 
+
+# open a new text file with "write" mode.
+file = open("results\\report.txt", "w")
+
+# write the results to the text file
+file.write("Financial Analysis\n")
+file.write("-" * 30)
+file.write(f"\nTotal Months: {total_months}\n")
+file.write(f"Total: ${profit_loss}\n")
+file.write(f"Average Change: ${average_change}\n")
+file.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n")
+file.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})\n")
+# make sure the file is closed
+file.close()
