1b73098ef424b7ff3b18ab58a1541279f0a43013
Karl Ramsay <nytechtiger@gmail.com>
2020-02-20 02:12:51 -0500

    initial code build for PyParagraph solution

---
+++ PyParagraph/main.py
@@ -0,0 +1,66 @@
+# import modules
+import os, csv, re
+
+letter_count = 0.0
+avg_letter_count = 0.0
+avg_sentence_len = 0.0
+
+#declare a list array for the tokenized paragraph
+tokenized_paragraph = []
+
+#declare a list array for the tokenized sentences
+sentence_len = []
+
+#declare a string variable for the clean paragraph
+clean_paragraph = ""
+
+# set path for source file
+srcpath = os.path.join("data", "paragraph.txt")
+
+# open source files as readonly, and instantiate a reader object
+with open(srcpath, 'r', newline='') as txtfile:
+  for sentences in txtfile:
+    #load the sentences into the clean_paragraph String
+    clean_paragraph = sentences
+
+# once we have paragraph loaded as text, determine the number of words per sentence
+# initialize a counter for each character in the clean text string
+l = 0
+
+# initialize a counter for numbe rof words
+w = 1 
+# loop through the clean_paragraph string letter by letter
+for l in range(len(clean_paragraph)):
+
+  if clean_paragraph[l] == " ":
+
+    # if we hit a space then we found another word so add 1 to the word count total
+    w += 1
+
+  elif clean_paragraph[l] == ".":
+    
+    # we hit a period then it means the sentence is done, so append the word count total to a sentence_len list array
+    sentence_len.append(int(w))
+    
+    # reset the wordc count 
+    w = 0
+
+# use the regular expressions module to tokenize the paragraph into an array of words
+tokenized_paragraph = re.split(r'\s', clean_paragraph)
+
+for word in tokenized_paragraph:
+  letter_count += len(word)
+
+# create and set variables needed for stats in results summary
+approx_word_count = len(tokenized_paragraph)
+approx_sentence_count = clean_paragraph.count(".")
+avg_letter_count = format((letter_count / len(tokenized_paragraph)),".2f")
+avg_sentence_len = format(sum(sentence_len) / len(sentence_len), ".1f")
+
+# Print Results
+print("Paragrah Analysis")
+print("--" * 9)
+print(f"Approximate Word Count: {approx_word_count}")
+print(f"Approximate Sentence Count: {approx_sentence_count}")
+print(f"Average Letter Count: {avg_letter_count}")
+print(f"Average Sentence Count: {avg_sentence_len} ")
---
+++ PyParagraph/readme.md
@@ -0,0 +1,38 @@
+![A boss knuckle image](boss.jpg)
+# PyParagraph Solution
+
+The included script provides a solution to ingest a .txt file into a string variable and process the file to analyze key meta data about the document. <br/><br/>
+
+In summary, the  follow statistics are needed about the txt file: <br/>
+
+* An approximation of the Word Count for the ingested file.
+
+* An approximation of the sentence count in the ingested file.
+
+* An approximation of the average Letter Count.
+
+* An Approximation of the average length of each sentence.
+
+## Approach
+
+### Modules and Libraries used
+In addition to using the base python modules and libraries, I imported the **os**,**csv** and **re** modules to resolve building a file path in a platform independent manner as well as reading and processing the source txt file.<br/>
+
+### Pseudo Code
+
+* declare a list array for the tokenized paragraph
+* declare a list array for the tokenized sentences
+* declare a string variable for the clean paragraph
+* set path for source file
+* open source files as readonly, and instantiate a reader object
+* load the sentences into the clean_paragraph String
+* once we have paragraph loaded as text, determine the number of words per sentence
+* initialize a counter for each character in the clean text string
+* initialize a counter for numbe rof words
+* loop through the clean_paragraph string letter by letter
+* if we hit a space then we found another word so add 1 to the word count total
+* we hit a period then it means the sentence is done, so append the word count total to a 
+* reset the wordc count 
+* use the regular expressions module to tokenize the paragraph into an array of words
+* create and set variables needed for stats in results summary
+* Print Results
